8085f257c3eef59320b65db5ed531d27970b41ff
slackjaww <yac@live.com.ar>
2020-02-15 05:19:34 -0300

    index del juego con plantilla main.blade.php

--- Laravel-DH-Proyecto/resources/views/welcome.blade.php
+++ Laravel-DH-Proyecto/resources/views/welcome.blade.php
@@ -1,99 +1,82 @@
+	{{--
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>El título de mi página</title>
+	<link rel="stylesheet" href="style.css">
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+</head>
+<body>
+	<header>
+		<h1>Header</h1>
+	</header>
+	<!-- Aquí empieza el contenido principal de nuestra página -->
+	<main>
+		<h2>Relacionado</h2>
+	</main>
+	<footer>
+		<p>©Copyright 2050 by nobody. All rights reversed.</p>
+	</footer>
+</body>
+</html>--}}
+@extends('layout.main')
 
-        <title>Laravel</title>
+@section('imagenDeFondo', 'images/fondo-web.jpg')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+@section('main')
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+	<div class="container my-5">
 
-            .full-height {
-                height: 100vh;
-            }
+		<div class="row d-flex align-items-center my-5 py-5">
+			<div class="col-md-7">
+				<h2 class="display-4">Título del beneficio 1. <br><span class="text-muted">Sub titulo.</span></h2>
+				<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis
+					euismod
+					semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus
+					ac cursus
+				commodo.</p>
+			</div>
+			<div class="col-md-5">
+				<img class=" img-fluid mx-auto" src="images/cards1.png" alt="Generic placeholder image">
+			</div>
+		</div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+		<hr class="" id="beneficio2">
 
-            .position-ref {
-                position: relative;
-            }
+		<div class="row d-flex align-items-center my-5 py-5">
+			<div class="col-md-5">
+				<img class=" img-fluid mx-auto" src="images/cards2.png" alt="Generic placeholder image">
+			</div>
+			<div class="col-md-7">
+				<h2 class="display-4">Título del beneficio 2. <br><span class="text-muted">Sub titulo.</span></h2>
+				<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis
+					euismod
+					semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus
+					ac cursus
+				commodo.</p>
+			</div>
+		</div>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+		<hr class="" id="beneficio3">
 
-            .content {
-                text-align: center;
-            }
+		<div class="row d-flex align-items-center my-5 py-5">
+			<div class="col-md-7">
+				<h2 class="display-4">Título del beneficio 3. <br><span class="text-muted">Sub titulo.</span></h2>
+				<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis
+					euismod
+					semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus
+					ac cursus
+				commodo.</p>
+			</div>
+			<div class="col-md-5">
+				<img class=" img-fluid mx-auto" src="images/cards3.png" alt="Generic placeholder image">
+			</div>
+		</div>
+	</div>
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+	<section class="call-to-action">
+		<h2 class="tex-cta">Empeza a jugar ahora!!</h2>
+		<button type="button" class="btn btn-cta btn-primary btn-lg btn-warning"><a href="login.php">COMENZAR</a></button>
+	</section>
+	@endsection
