49a7d08d6fb387324d13c04446931354a0053b4c
ArielJans <arjapro@gmail.com>
2020-02-19 16:15:24 -0300

    Correccion vista juego

--- Laravel-DH-Proyecto/resources/views/questionrace/juego.blade.php
+++ Laravel-DH-Proyecto/resources/views/questionrace/juego.blade.php
@@ -3,39 +3,33 @@
 
 @section('content')
 
-<div class="container my-5">
-    <div id="portada">
-    <h2>PUNTAJE: {{ session()->get('puntaje', 0) }} </h2>
+<div class="row">
 
-    <form id="juego" action="/juego/next" method="POST">
-    @csrf
-        {{$pregunta->pregunta}}
-        <br>
-        <input type="hidden" name="pregunta_id" value="{{$pregunta->id}}">
-        @foreach($respuestas as $key => $respuesta)
-        <div style="padding-left: 45%">
-        <div class="text-left">
-        <input name="rta" type="radio" value="{{$respuestas[$random[$key]]->respuesta}}"/>
-        <span class="text-dark">{{$respuestas[$random[$key]]->respuesta}}</span><br>
-        </div>
-        </div>
-       @endforeach
-        <br>
-        <br>
-        <input type="submit" name="enviar" id="btnjuego"> <br>
-        <div id="rta">
-
-        </div>
-    </form>
-
-</div>
-        <!-- ///////////////// IMAGENES RIGTH ///////////////-->
-        <section class="rigth col-md-8">
-          <div class="header__bajada">
-
+    <div id="portada" class="left col-md-4">
+        <h2>PUNTAJE: {{ session()->get('puntaje', 0) }} </h2>
+        <form id="juego" action="/juego/next" method="POST">
+            <h2 class="call-to-action">Pregunta #1</h2>
+            @csrf
+            {{$pregunta->pregunta}}
+            <br>
+                <input type="hidden" name="pregunta_id" value="{{$pregunta->id}}">
+            @foreach($respuestas as $key => $respuesta)
+                <div class="text-left">
+                    <input name="rta" type="radio" value="{{$respuestas[$random[$key]]->respuesta}}"/>
+                    <span class="text-dark">{{$respuestas[$random[$key]]->respuesta}}</span><br>
+                </div>
+        @endforeach
+            <br>
+            <button class="btn btn-primary" type="submit" name="enviar" id="btnjuego">Enviar</button>
+        </form>
     </div>
+
+    <!-- ///////////////// IMAGENES RIGTH ///////////////-->
+        <section class="rigth col-md-8">
             <img src="images/fondo.jpg" class="img-back" alt="Imagen de fondo QUESTION RACE">
             <img src="images/medal.png" class="img-ficha" alt="ficha de juego">
         </section>
-    </section>
+        <!-- ///////////////// FIN IMAGENES RIGTH ///////////////-->
+</div>
+
 	@endsection
