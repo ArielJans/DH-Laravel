e595a5d892e7d13fb10c03f1a3aac9546d912e39
ArielJans <arjapro@gmail.com>
2020-02-19 16:57:39 -0300

    Correccion footer vista/css

--- Laravel-DH-Proyecto/resources/views/questionrace/juego.blade.php
+++ Laravel-DH-Proyecto/resources/views/questionrace/juego.blade.php
@@ -5,6 +5,7 @@
 
 <div class="row">
 
+    <!-- ///////////////// LEFT ///////////////-->
     <div id="portada" class="left col-md-4">
         <h2>PUNTAJE: {{ session()->get('puntaje', 0) }} </h2>
         <form id="juego" action="/juego/next" method="POST">
@@ -18,18 +19,19 @@
                     <input name="rta" type="radio" value="{{$respuestas[$random[$key]]->respuesta}}"/>
                     <span class="text-dark">{{$respuestas[$random[$key]]->respuesta}}</span><br>
                 </div>
-        @endforeach
+            @endforeach
             <br>
             <button class="btn btn-primary" type="submit" name="enviar" id="btnjuego">Enviar</button>
         </form>
     </div>
+    <!-- /////////////////FIN LEFT ///////////////-->
 
     <!-- ///////////////// IMAGENES RIGTH ///////////////-->
-        <section class="rigth col-md-8">
-            <img src="images/fondo.jpg" class="img-back" alt="Imagen de fondo QUESTION RACE">
-            <img src="images/medal.png" class="img-ficha" alt="ficha de juego">
-        </section>
-        <!-- ///////////////// FIN IMAGENES RIGTH ///////////////-->
+    <section class="rigth col-md-8">
+        <img src="images/fondo.jpg" class="img-back" alt="Imagen de fondo QUESTION RACE">
+        <img src="images/medal.png" class="img-ficha" alt="ficha de juego">
+    </section>
+    <!-- ///////////////// FIN IMAGENES RIGTH ///////////////-->
 </div>
 
-	@endsection
+@endsection
