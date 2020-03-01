14cccb4235c2d066560f09cd2724adc447fbf173
slackjaww <yac@live.com.ar>
2020-03-01 13:47:44 -0300

    modif view de juego terminado

--- Laravel-DH-Proyecto/resources/views/questionrace/resultado.blade.php
+++ Laravel-DH-Proyecto/resources/views/questionrace/resultado.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.app')
 @section('styles','css/styles-juego.css')
 
-@section('content')
 
-<div id="portada" class="container py-5">
+<section class="img-header" style="background-image: url('./images/fondoFinal.jpg'); height: 100%;
+	
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;">
+
+@section('content')
+<div id="portada" class="container py-5 text-center card" style="position: center; margin-top: 9rem; background-color: rgba(255,255,255, 0.6) !important;">
             <h1><?= "EL JUEGO HA TERMINADO"?></h1>
             <h2>USUARIO: {{ Auth::user()->user }}</h2>
             <h2>SU PUNTAJE ES: {{ session()->get('puntaje', 0) }} </h2><br>
-            <button class="btn btn-primary" id="cb2" for="cb1"><a id="start" href="/juego" onclick="{{session(['puntaje' => 0])}}"></a>Juego Nuevo</button>
+            <button type="button" class="btn btn-primary btn-lg btn-block" onclick="window.location.href='/juego' {{session(['puntaje' => 0])}}">Jugar de nuevo!</button>
 </div>
-
+</section>
 @endsection
+
--- Laravel-DH-Proyecto/routes/web.php
+++ Laravel-DH-Proyecto/routes/web.php
@@ -20,7 +20,7 @@ Route::get('/ranking', 'RankingController@listar')->name('questionrace.ranking')
 
 Route::get('/juego', 'JuegoController@traerDatos')->middleware('auth');
 
-Route::post('/juego', 'JuegoController@verificacion');
+
 Route::post('/juego', 'JuegoController@verificacion'); //El juego pasa de pregunta si es correcta;
 
 //LISTAR PREGUNTAS Y RESPUESTAS
