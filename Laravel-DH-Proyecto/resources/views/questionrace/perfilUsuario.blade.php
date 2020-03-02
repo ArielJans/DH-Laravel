d55e7b284b16c56f1c9d762c0794b75839499cb0
slackjaww <yac@live.com.ar>
2020-03-02 03:49:47 -0300

    modf perfil

--- Laravel-DH-Proyecto/resources/views/questionrace/perfilUsuario.blade.php
+++ Laravel-DH-Proyecto/resources/views/questionrace/perfilUsuario.blade.php
@@ -7,7 +7,8 @@
     <section class="row">
         <section class="left col-md-4">
             <h1 class="name">{{ Auth::user()->user }}</h1>
-            <img src="storage/{{ Auth::user()->imagen }}" alt="Imagen de perfil">
+            <img src="storage/@if(Auth::user()->imagen == ''){{'perfilGenerico.jpg'}}
+            @else{{Auth::user()->imagen}}@endif" alt="Imagen de perfil">
             <p>País: <strong>Argentina</strong></p>
             <button type="button" class="btn btn-perfil btn-primary">Cambiar avatar</button>
         </section>
