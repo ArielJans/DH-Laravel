055b9dc46dd95f1b5e8b181957d9fc1dce81f6f9
slackjaww <yac@live.com.ar>
2020-03-06 13:04:50 -0300

    modf

--- Laravel-DH-Proyecto/resources/views/questionrace/resultado.blade.php
+++ Laravel-DH-Proyecto/resources/views/questionrace/resultado.blade.php
@@ -12,8 +12,8 @@
 <div id="portada" class="container py-5 text-center card" style="position: center; margin-top: 9rem; background-color: rgba(255,255,255, 0.6) !important;">
             <h1 class="won"><?= "EL JUEGO HA TERMINADO"?></h1>
             <h2 class="custex">Usuario: {{ Auth::user()->user }}</h2>
-            <h2 class="custex">Tu puntaje es: {{ session()->get('puntaje', 0) }} </h2>
-            <h2>Su mejor puntaje es: {{ session()->get('mejorPuntaje', 0) }} </h2><br>
+            <h2 class="custex">Puntaje obtenido: {{ session()->get('puntaje', 0) }} </h2>
+            <h2 class="custex">Su mejor puntuación registrada: {{ session()->get('mejorPuntaje', 0) }} </h2><br>
             <button type="button" class="btn btn-primary btn-lg btn-block" onclick="window.location.href='/juego' {{session(['puntaje' => 0])}}">Jugar de nuevo!</button>
 </div>
 </section>
