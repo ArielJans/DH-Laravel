c2e7f87917d8d250beec93269415ca050d2e6c11
ArielJans <arjapro@gmail.com>
2020-03-10 19:26:33 -0300

    Mejora de estilos perfil, cambio imagen juego

--- Laravel-DH-Proyecto/resources/views/questionrace/perfilUsuario.blade.php
+++ Laravel-DH-Proyecto/resources/views/questionrace/perfilUsuario.blade.php
@@ -7,26 +7,25 @@
     <section class="row">
         <section class="left col-md-4">
             <h1 class="name">{{ Auth::user()->user }}</h1>
-            <img src="storage/@if(Auth::user()->imagen == ''){{'perfilGenerico.jpg'}}
+            <img class="perfil" src="./images/@if(Auth::user()->imagen == ''){{'perfilGenerico.jpg'}}
             @else{{Auth::user()->imagen}}@endif" alt="Imagen de perfil">
             <p>País: <strong>Argentina</strong></p>
             <button type="button" class="btn btn-perfil btn-primary">Cambiar avatar</button>
         </section>
 
         <section class="col-md-8">
-        <form class="card">
-            <label>Usuario</label>
-            <input type="text" value="{{ Auth::user()->user }}" class="col-md-12">
-            <label>Nombre</label>
-            <input type="text" value="{{ Auth::user()->nombre }}" class="col-md-12">
-            <label>Correo</label>
-            <input type="text" value="{{ Auth::user()->email }}" class="col-md-12">
-            <label>
-                <button class="btn btn-primary">Actualizar datos</button>
-            </label>
-        </form>
+            <form class="card card_perfil">
+                <label class="labelCustom">Usuario</label>
+                <input type="text" class="form-control" value="{{ Auth::user()->user }}" class="col-md-12">
+                <label class="labelCustom">Nombre</label>
+                <input type="text" class="form-control" value="{{ Auth::user()->nombre }}" class="col-md-12">
+                <label class="labelCustom">Correo</label>
+                <input type="text" class="form-control" value="{{ Auth::user()->email }}" class="col-md-12">
+                <label>
+                    <button class="btn btn-primary btn_act_datos">Actualizar datos</button>
+                </label>
+            </form>
         </section>
     </section>
-
 
 @endsection
